8943223a84217a3372a6d635da01bbc9828a9c60
harazono <6312081@ed.tus.ac.jp>
2020-04-08 15:44:58 +0900

    WIP

--- ContextDepententErrorEstimater.py
+++ ContextDepententErrorEstimater.py
@@ -12,10 +12,10 @@ if __name__ == '__main__':
 	parser.add_argument("sorted_bam",       help = "Aligned reads to be counted. BAM should be sorted by position")
 	parser.add_argument("-k", "--kmer",  help = "k-mer size. Default is 0", default = 0, type = int)
 	parser.add_argument("-c", "--chr",   help = "chromosome or contig to be considered", default = "")
-	parser.add_argument("-s", "--start", help = "start position of the region", type = int)
+	parser.add_argument("-s", "--start", help = "start position of the region", default = 0, type = int)
 	parser.add_argument("-e", "--end",   help = "end   position of the region", type = int)
 	parser.add_argument("--json",        help = "If this arg is set then output results in JSON format.", action = 'store_true')
-
+	parser.add_argument("-v", "--verbose", help = "Outputs verbose", action = 'store_true')
 	args = parser.parse_args()
 
 	ref_file_path = args.reference_genome
@@ -29,11 +29,24 @@ if __name__ == '__main__':
 	sam           = pysam.AlignmentFile(bam_file_path, "rb")
 	reference     = ref.references[0]
 	context_readbase_dict = {}
+	is_verbose    = args.verbose
 
+	reference_size = len(ref.fetch(reference = reference))
 	if chrm != "":
 		reference = chrm
 
+	if is_verbose:
+		print("Reference name:\t%s\nReference len :\t%s"%(reference, reference_size), file = sys.stderr)
+		if end_pos != None:
+			len_of_region = end_pos - start_pos
+		else:
+			len_of_region = reference_size - start_pos
+
+	roop_counter = 0
 	for pileupcolumn in sam.pileup(contig = reference, start = start_pos, stop = end_pos, truncate = True):
+		if pileupcolumn.pos - k <= 0 or pileupcolumn.pos + 1 + k >= reference_size:
+			continue
+
 		refbase = ref.fetch(reference = reference, start = pileupcolumn.pos, end = pileupcolumn.pos + 1)
 		refcon  = ref.fetch(reference = reference, start = pileupcolumn.pos - k, end = pileupcolumn.pos + 1 + k)
 
@@ -47,6 +60,9 @@ if __name__ == '__main__':
 					context_readbase_dict[refcon][base] += 1
 			if pileupread.is_del:
 				context_readbase_dict[refcon]["x"] += 1
+		if is_verbose:
+			roop_counter += 1
+			print("%s/%s proceeded\r"%(roop_counter, len_of_region), file = sys.stderr, end = "")
 
 	if is_json:
 		pp.pprint(json.dumps(context_readbase_dict))
