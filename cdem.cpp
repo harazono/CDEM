5fa4e3598d739db27fdaef2355f031cd1fcfadf2
harazono <harazono_yoritaka_17@stu-cbms.k.u-tokyo.ac.jp>
2020-04-29 16:31:45 +0900

    WIP

--- cdem.cpp
+++ cdem.cpp
@@ -10,11 +10,7 @@
 #include <omp.h>
 #include <stdexcept>
 #include "cpas_debug.h"
-
-//#include "htslib/cram/cram.h"
-#include "htslib/htslib/hts.h"
-#include "htslib/htslib/sam.h"
-#include "htslib/htslib/vcf.h"
+#include "cpas_tsv.h"
 
 
 using namespace std;
@@ -26,8 +22,8 @@ void printUsageAndExit(){
 }
 
 int main(int argc, char *argv[]){
-
-/*  GDB_On_SEGV g(argv[0]);
+/*
+  GDB_On_SEGV g(argv[0]);
   struct option longopts[] = {
     { "csv"       , no_argument       , NULL , 'c' } ,
     // { "delete" , optional_argument , NULL , 'd' } ,
@@ -66,19 +62,8 @@ int main(int argc, char *argv[]){
   const int NUM_REQUIRED_ARGUMENTS = 2;
   if(optind + NUM_REQUIRED_ARGUMENTS != argc) {
     printUsageAndExit();
-  }
-*/
-  const int    optind = 0;
-  const char* reference_genome_filename = argv[optind + 0];
-  const char* aligned_reads_filename    = argv[optind + 1];
-  htsFile *in_sam;
-  in_sam = sam_open(aligned_reads_filename, "r");
-  if(in_sam == NULL){
-    fprintf(stderr, "failed to open %s\n", aligned_reads_filename);
-    return 2;
-  }
-  sam_close(in_sam);
-  string name_of_the_only_reference_sequence;
+  }*/
+
   return 0;
 }
 
